3d3ac15926fddc12c9abd5697b1eb81d4d780928
David Jones <david.jones@coveainsurance.co.uk>
2023-02-07 23:46:14 +0000

    feat: first token smart contract

--- token-solution.sol
+++ token-solution.sol
@@ -2,7 +2,33 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract CryptoToken {
-    uint private totalSupply = 7;
+    uint private totalSupply = 1000;
     address public sender;
     address public minter;
+
+    mapping(address => uint) public balances;
+
+    event Sent(address from, address to, uint amount);
+
+    constructor(){
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        require(amount < totalSupply);
+
+        balances[receiver] += amount;
+        
+        totalSupply -= amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+        
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+
+        emit Sent(msg.sender, receiver, amount);
+    }
 }
