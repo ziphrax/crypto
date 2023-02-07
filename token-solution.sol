56080241e5a12fd971c4b01f38158f3a6c5ccb04
David Jones <david.jones@coveainsurance.co.uk>
2023-02-07 22:57:01 +0000

    first commit

---
+++ token-solution.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract CryptoToken {
+    uint private totalSupply = 7;
+    address public sender;
+    address public minter;
+}
